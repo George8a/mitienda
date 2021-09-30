e54ea69916b09274b065f1c1dd8057f6fda84c7c
Jose Gomez <90806666+Y4cho@users.noreply.github.com>
2021-09-30 06:13:55 -0500

    Se actualiza la clase categoria

--- mi_tienda/src/main/java/logica/categoria.java
+++ mi_tienda/src/main/java/logica/categoria.java
@@ -63,9 +63,68 @@ public class categoria {
             return false;
         }
     }
+    
+    public boolean actualizarcategoria() {
+        Conexion conexion = new Conexion();
+        String Sentencia = "UPDATE `categorias` SET nombre='" + this.nombre + "',descripcion='" + this.descripcion + "' WHERE id_categoria=" + this.id_categoria + ";";
+        if (conexion.setAutoCommitBD(false)) {
+            if (conexion.actualizarBD(Sentencia)) {
+                conexion.commitBD();
+                conexion.cerrarConexion();
+                return true;
+            } else {
+                conexion.rollbackBD();
+                conexion.cerrarConexion();
+                return false;
+            }
+        } else {
+            conexion.cerrarConexion();
+            return false;
+        }
+    }
+    
+    public List<categoria> listarcategorias() throws SQLException {
+        Conexion conexion = new Conexion();
+        List<categoria> listacategorias = new ArrayList<>();
+        String sql = "select * from categorias order by identificacion asc";
+        ResultSet rs = conexion.consultarBD(sql);
+        categoria c;
+        while (rs.next()) {
+            c = new categoria();
+            c.setId_categoria(rs.getInt("id_categoria"));
+            c.setnombre(rs.getString("nombre"));
+            c.setdescripcion(rs.getString("descripcion"));
+            listacategorias.add(c);
 
-    private categoria getcategoria() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        conexion.cerrarConexion();
+        return listacategorias;
+    }
+    
+    public categoria getcategoria() throws SQLException {
+        Conexion conexion = new Conexion();
+        String sql = "select * from categorias where id_categoria='" + this.id_categoria + "'";
+        ResultSet rs = conexion.consultarBD(sql);
+        if (rs.next()) {
+            this.id_categoria = rs.getInt("id_categoria");
+            this.nombre = rs.getString("nombre");
+            this.descripcion = rs.getString("descripcion");
+            conexion.cerrarConexion();
+            return this;
+
+        } else {
+            conexion.cerrarConexion();
+            return null;
+        }
+
+    }
+
+    private void setnombre(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    private void setdescripcion(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); 
     }
     
     
